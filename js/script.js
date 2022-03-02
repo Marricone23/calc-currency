abed3f9389d6d41d2f8f0a1a1d4771209e180543
Marricone23 <68504252+Marricone23@users.noreply.github.com>
2022-03-02 15:15:02 +0300

    feat: update script

--- js/script.js
+++ js/script.js
@@ -12,22 +12,13 @@ request.setRequestHeader('Content-type','application/json; charset=utf-8' );
 request.send();
 
 request.addEventListener('load',() => {
-    if(request.status === 200){
-        //console.log(request.response);
+    if(request.status === 200{   
         const data = JSON.parse(request.response);
         inputUSD.value = (+inputRub.value / data.current.usd).toFixed(2);
     }else{
         inputUSD.value = "Что-то пошло не так";
         }    
 });
-
-
-//status
-//statusText
-//response
-//readyState
-
-
 
 });
 
